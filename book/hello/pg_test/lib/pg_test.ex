26c9133e267fa9a90774eeaf7f33b86c27bfc61b
Don Browning <don.browning@gmail.com>
2016-03-03 20:26:48 -0500

    cleaned it up a figured a bunch of shit out

--- book/hello/pg_test/lib/pg_test.ex
+++ book/hello/pg_test/lib/pg_test.ex
@@ -1,13 +1,41 @@
 defmodule PgTest do
 
-	def register(user) do
-		{:ok, pid} = Postgrex.start_link(hostname: "localhost", username: "postgres", password: "postgres", database: "postgres")
+	def select_all() do
+		{:ok, pid} = connect
 
 		sql = "select * from employee;"
-		IO.puts(sql)
     	res = Postgrex.query!(pid, sql, [])
-    	[record | last] = res.rows
-    	IO.puts(record)
 
+    	Enum.each(res.rows, fn(r)
+    		-> 
+    			[name, dept, job] = r
+
+    			# not thinking this is idomatic
+    			n = String.strip(name)
+    			d = String.strip(dept)
+    			j = String.strip(job)
+
+    			IO.puts(n <> " is a " <> j <> " in the " <> d <> " department")
+    			
+    	end)
+
+    	length res.rows
+	end
+
+	def select_one(person) do
+		
+		{:ok, pid} = connect
+
+		sql = "select * from employee where Name = $1;"
+    	res = Postgrex.query!(pid, sql, [person])
+    	[record | _] = res.rows
+    	
+    	# had to do this to prevet the above from returning a
+    	# list of tuples rather than the tuples themselves???
+    	record
+	end
+
+	defp connect() do
+		{:ok, pid} = Postgrex.start_link(hostname: "localhost", username: "postgres", password: "postgres", database: "postgres")
 	end
 end
--- book/hello/pg_test/test/pg_test_test.exs
+++ book/hello/pg_test/test/pg_test_test.exs
@@ -3,7 +3,17 @@ defmodule PgTestTest do
   doctest PgTest
 
   test "is able to access database" do
-    PgTest.register({"test@test.com", "password"})
-    #assert res.success,res.message
+    len = PgTest.select_all()
+    
+    assert len == 4
+  end
+
+  test "is able to find Wilma" do
+  	ret = PgTest.select_one("Wilma Flinstone")
+  	
+  	# Do you really need to ignore the rest of the tuples?
+  	[name, _, _] = ret
+  	
+  	assert String.strip(name) == "Wilma Flinstone"
   end
 end
